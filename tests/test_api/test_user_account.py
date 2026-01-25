2bc1f89b650dd6d80afd39b9894cdd09e8b47425
fareena imran <fareenaimran999@gmail.com>
2026-01-25 17:29:34 +0500

    Update Account API tests

--- tests/test_api/test_user_account.py
+++ tests/test_api/test_user_account.py
@@ -1,5 +1,7 @@
 from api.end_points.account_api import AccountAPI
 from tests.fixtures.api_fixtures import create_account
+from tests.fixtures.api_fixtures import get_account_details
+
 
 def test_create_user_account(create_account):
     """
@@ -9,26 +11,18 @@ def test_create_user_account(create_account):
     user_data=create_account
     print(f"\nPOST: Account created with '{user_data['email']}'")
 
-def test_get_user_account_details(api_request_context,email="john.smith1769075170@example.com"):
+def test_get_user_account_details(api_request_context,get_account_details):
     """
     GET Test
     Get user account details by email
     """
-    account_api=AccountAPI(api_request_context)
-    response = account_api.get_user_detail_by("email",email)
+    email = "john.smith1769075170@example.com"
+    details=get_account_details(email)
 
-    assert response.status == 200, f"Failed to get user details,Expected 200 but got {response.status}"
+    # Verify user details by emai
+    print(f"\nUser Details: \n\n {details}")
 
-    # Verify user details by email
-    details = response.json()
-    if "email" in details["user"]:
-        assert details["user"]["email"] == email, "Email Mismatch!!"
-
-    print(f"\nUser with email: {email} exists in user records!")
-    print("\n\nUser Detail:\n")
-    for key,value in details["user"].items(): print(key,"-",value)
-
-def test_put_account_details(api_request_context,create_account):
+def test_put_account_details(api_request_context,create_account,get_account_details):
     """
     TEST PUT
     Test update user details
@@ -42,15 +36,14 @@ def test_put_account_details(api_request_context,create_account):
     updated_payload=user_data.copy()
     updated_payload["name"],updated_payload["city"],updated_payload["country"]="Fareena","Karachi","Pakistan"
 
-
     response=account_api.update_account_details(updated_payload)
     assert response.status==200, f"Failed to update user details,Expected 200 but got {response.status}"
 
     assert "updated" in response.json()["message"],"Unable to update user details"
     print(f'\n{response.json()["message"]}')
 
-    response=account_api.get_user_detail_by("email",email)
-    print(f'\n{response.json()["user"]}')
+    details=get_account_details(email)
+    print(f'\n{details}')
 
 def test_delete_user_account(api_request_context,create_account):
     """
